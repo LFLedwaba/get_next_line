0ad5443fcc645d6f7ee573b0c6d637012654e8c5
Frans LEDWABA <fledwaba@e4r15p9.wethinkcode.co.za>
2018-09-12 08:53:56 +0200

    get_next_line

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_get_next_line.h                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fledwaba <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/06 09:14:23 by fledwaba          #+#    #+#             */
+/*   Updated: 2018/06/17 10:48:01 by fledwaba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include "libft/libft.h"
+# define BUFF_SIZE 50
+
+int		get_next_line(const int fd, char **line);
+
+#endif
